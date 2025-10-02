176a04709d8c020e3c1f6fe9a430e0ee28b9ef41
luizfagner <luizfagnersilva87@gmail.com>
2025-10-02 20:38:40 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,5 +1,13 @@
 let api = "https://e016bfb4-74d4-4d1c-a0b3-22044a9a2089-00-1uygczbfi4zz0.kirk.replit.dev";
 
+async function gerarHash(texto) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(texto);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+}
+
 async function login() {
     const nome = document.getElementById("nome").value;
     const senha = document.getElementById("nome").value;
@@ -12,7 +20,7 @@ async function login() {
         });
 
         const data = await response.json();
-        
+
         if (response.status === 200) {
             console.log("Sucesso ao fazer login");
             window.location.href = "listar.html";
